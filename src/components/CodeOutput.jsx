fe14bc99f40b4e71e450b15acd7b717fc82b5bab
Gustavo <unmail@mail.com>
2025-01-19 19:15:11 -0300

    agrego boton para copiar el codigo css al portapapeles

--- src/components/CodeOutput.jsx
+++ src/components/CodeOutput.jsx
@@ -1,9 +1,14 @@
-import { Box, Paper, Typography } from "@mui/material"
+import { useState } from "react";
+import { Box, IconButton, Paper, Typography } from "@mui/material"
+import { FaRegCopy, FaCheck } from "react-icons/fa";
 import SyntaxHighlighter from "react-syntax-highlighter"
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
 export const CodeOutput = ({ buttonStyle }) => {
+
+  const [isCopied, setIsCopied] = useState(false)
+
   const generateCSS = () => {
     const background = buttonStyle.gradient
       ? `background: linear-gradient(${buttonStyle.gradientAngle}deg, ${buttonStyle.gradientColor1}, ${buttonStyle.gradientColor2});`
@@ -27,14 +32,33 @@ export const CodeOutput = ({ buttonStyle }) => {
     }`;
   };
 
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(generateCSS())
+      console.log('Texto copiado al portapapeles')
+      setIsCopied(true)
+      setTimeout(() => setIsCopied(false), 3000)
+    }
+      catch (err) {
+      console.error('Failed to copy: ', err)
+    }
+  }
 
   return (
     <Box>
       <Typography variant="h6" gutterBottom>Generate CSS Code</Typography>
-      <Paper elevation={3} sx={{ bgcolor: '#1e1e1e' }}>
-        <SyntaxHighlighter language="css" style={atomOneDark} customStyle={{ margin: 0, padding: '20px' }}>
+      <Paper elevation={3} sx={{ bgcolor: '#1e1e1e', position: 'relative' }}>
+        <SyntaxHighlighter language="css" style={atomOneDark} customStyle={{ margin: 0, padding: '20px'}} >
           {generateCSS()}
         </SyntaxHighlighter>
+        
+          <IconButton onClick={handleCopy} sx={{ position: 'absolute', top: '8px', right: '8px' }}>
+            { isCopied ? (
+              <FaCheck size={20} className="text-green-500" style={{ transition: 'all 0.2s ease-in-out', color: 'grey' }} />
+            ) : (
+              <FaRegCopy size={20} style={{ transition: 'all 0.2s ease-in-out', color: 'grey' }} />
+            )}
+          </IconButton>
       </Paper>
     </Box>
   )
